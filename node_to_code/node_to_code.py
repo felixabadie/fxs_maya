a5f8830caeed97417928eb4c9c7810b9980f74ad
Felix <felix.max.abadie@gmail.com>
2026-07-29 16:50:00 +0200

    working mayaaskii exporter

--- node_to_code/node_to_code.py
+++ node_to_code/node_to_code.py
@@ -17,6 +17,21 @@ class TextFieldHelper:
         self.obj = sel[0]
 
 
+class FileFieldHelper:
+    def __init__(self, label, buttonLabel="Select", text="No file path selected"):
+        self.control = pm.textFieldButtonGrp(
+            label=label, buttonLabel=buttonLabel, text=text,
+            buttonCommand=self.set_path
+        )
+
+    def set_path(self):
+        filepath = pm.fileDialog2(fileMode=3)
+        if not filepath:
+            pm.warning("Warning, no file path selected")
+            return
+        self.obj = filepath[0]
+
+
 class NodeToCode:
     """
     Interface for node to code
@@ -29,9 +44,11 @@ class NodeToCode:
 
         with pm.window(self.win_id, title="Node to Code exporter") as win:
             with pm.columnLayout(adj=True):
-                pm.text(label="Select whatever you want to export. \nIf nothing is selected everything gets exported")
+                pm.text(label="Select File location", align="left")
+                self.file_path = FileFieldHelper("Save Directory: ")
+                pm.text(label="Select whatever you want to export. \nIf nothing is selected everything gets exported", align="left")
                 self.module_name = TextFieldHelper("Node module name: ")
-                pm.text(label="Please input a valid and unique name")
+                pm.text(label="Please input a valid and unique name", align="left")
                 with pm.horizontalLayout():
                     pm.button(label="Cancel")
                     pm.button(label="OK", command=self.execute)
@@ -42,7 +59,7 @@ class NodeToCode:
 
     def execute(self, *args):
 
-        deselect = [
+        deselect_list = [
             "persp", 
             "top", 
             "front", 
@@ -68,17 +85,22 @@ class NodeToCode:
         if not sel:
             pm.select(all=True)
 
-            for node in deselect:
+            for node in deselect_list:
                 try:
-                    pm.select(deselect=node)
+                    pm.select(node, deselect=True)
                 except Exception as e:
                     print(f"Cannot deselect {node}, reason: {e}")
                     continue
 
-        pm.exportSelected(exportPath=None, constraints=True, expressions=True, shader=True, preserveReferences=True, type="mayaAscii")
+        full_export_path = self.file_path.obj + "/" + self.module_name.control.getText()
+        print(f"File path: {full_export_path}")
 
-            
+        try:
+            pm.exportSelected(exportPath=full_export_path, constraints=True, expressions=True, shader=True, preserveReferences=True, type="mayaAscii")
+            print(f"Node network successfully save to: {full_export_path}")
 
-
+        except Exception as e:
+            print(f"Export failed: {e}")
+            pass
 
 NodeToCode()
